fd01b4f6fc89de154f6aebb5c4451376627f8f9b
zihaozhang9 <zihaozhang9@foxmail.com>
2019-09-23 10:56:42 +0800

    Create process.sh

---
+++ processdata/shapedata/process.sh
@@ -0,0 +1,5 @@
+pip install git+git://github.com/waspinator/pycococreator.git@0.2.0
+pip install tqdm
+wget https://patrickwasp.com/wp-content/uploads/2018/04/shapes_train_dataset.zip
+unzip shapes_train_dataset.zip
+python mask_to_coco.py
