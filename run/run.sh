1f855959fedc0b6ff4237c3e8c415376c5c181aa
zihaozhang9 <zihaozhang9@foxmail.com>
2019-09-23 09:29:39 +0800

    Create run.sh

---
+++ run/run.sh
@@ -0,0 +1,9 @@
+pip install git+git://github.com/waspinator/pycococreator.git@0.2.0
+pip install tqdm
+cd processdata/
+bash process.sh
+cd ../../
+rm -rf work_dirs/mask_rcnn_r50_fpn_1x
+./tools/dist_train.sh configs/mask_rcnn_r50_fpn_1x.py 1 #--resume_from work_dirs/mask_rcnn_r50_fpn_1x/latest.pth  #--validate
+python test/test.py
+python show/out.py
