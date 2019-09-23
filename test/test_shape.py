c8432a52c3149ca521b7e5738a44407cd22cd8db
zihaozhang9 <zihaozhang9@foxmail.com>
2019-09-23 13:03:40 +0800

    Update test_shape.py

--- test/test_shape.py
+++ test/test_shape.py
@@ -9,17 +9,17 @@ def create_dir(dst):
     os.makedirs(dst) 
 def show_image(model,imgpath,savepath):
     result = inference_detector(model, imgpath)
-    show_result(imgpath, result, model.CLASSES, out_file=savepath)
+    show_result(imgpath, result, model.CLASSES,show=False, out_file=savepath)
 def show_images(model,imgdir,savedir):
     for imgname in os.listdir(imgdir):
         imgpath = osp.join(imgdir,imgname)
         savepath = osp.join(savedir,imgname)
         show_image(model,imgpath,savepath)
 
-config_file = '/mmdetection/configs/mask_rcnn_r50_fpn_1x_shape.py'
-checkpoint_file ='/mmdetection/work_dirs/mask_rcnn_r50_fpn_1x_shape/latest.pth'#'epoch_2.pth'
-img_root = '/data/shape/shapes_train2018/'
-save_path = '/mmdetection/test/outputs_shape'
+config_file = '../configs/my/mask_rcnn_r50_fpn_1x_shape.py'
+checkpoint_file ='../work_dirs/mask_rcnn_r50_fpn_1x_shape/latest.pth'
+img_root = '../processdata/shapedata/shapes/train/shapes_train2018/'
+save_path = 'outputs_shape'
 create_dir(save_path)
 
 model = init_detector(config_file, checkpoint_file, device='cuda:0') 
