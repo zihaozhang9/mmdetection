6b23fa75cde10a7866a61d521fdc6391186f4aef
zihaozhang9 <zihaozhang9@foxmail.com>
2019-09-23 09:42:57 +0800

    Update run_shape.sh

--- run/run_shape.sh
+++ run/run_shape.sh
@@ -1,10 +1,12 @@
 pip install git+git://github.com/waspinator/pycococreator.git@0.2.0
 pip install tqdm
 cd processdata/shapedata/
-#python process.py
+#python mask_to_coco.py
 cd ../../
 #python tools/train.py 
 rm -rf work_dirs/mask_rcnn_r50_fpn_1x_shape
-./tools/dist_train.sh configs/mask_rcnn_r50_fpn_1x_shape.py 1 #--resume_from work_dirs/mask_rcnn_r50_fpn_1x_shape/latest.pth --validate
+#./tools/dist_train.sh configs/mask_rcnn_r50_fpn_1x_crertor.py 1 --validate
+./tools/dist_train.sh configs/mask_rcnn_r50_fpn_1x_shape.py 1 --validate
+#./tools/dist_train.sh configs/mask_rcnn_r50_fpn_1x.py 1
 #python test/test_shape.py
 #python outhtml.py
